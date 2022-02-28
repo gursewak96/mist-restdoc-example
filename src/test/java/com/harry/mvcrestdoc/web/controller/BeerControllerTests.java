d9008a53b777af049fb701043ad5ada1544eb3d1
gursewak96 <gursewak19d@gmail.com>
2022-02-28 10:59:29 +0530

    Documenting Query Parameter, closes #3

--- src/test/java/com/harry/mvcrestdoc/web/controller/BeerControllerTests.java
+++ src/test/java/com/harry/mvcrestdoc/web/controller/BeerControllerTests.java
@@ -24,10 +24,9 @@ import java.util.UUID;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
-import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 //import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(RestDocumentationExtension.class)
@@ -49,12 +48,18 @@ public class BeerControllerTests {
     void getBeerById() throws Exception {
         given(beerRepository.findById(any())).willReturn(Optional.of(Beer.builder().build()));
 
-        mockMvc.perform(get("/api/v1/beer/{beerId}", UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
+        mockMvc.perform(get("/api/v1/beer/{beerId}", UUID.randomUUID().toString())
+                        .accept(MediaType.APPLICATION_JSON)
+                        .param("isBeerCold","yes")
+                )
                 .andExpect(status().isOk())
-                .andDo(document("v1/beer",pathParameters(
-                        parameterWithName("beerId")
-                                .description("UUID of desired beer to get.")
-                )));
+                .andDo(document("v1/beer",
+                        pathParameters(
+                            parameterWithName("beerId").description("UUID of desired beer to get.")),
+                        requestParameters(
+                                parameterWithName("isBeerCold").description("is Beer cold query parameter")
+                        )
+                ));
     }
 
     @Test
